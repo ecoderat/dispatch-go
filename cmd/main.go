6823e9e1f3dd5516e6b076fe6a65afa91c168f95
Murat Güngör <ecoderat@gmail.com>
2025-05-28 18:10:19 +0300

    [feat] Add basic routes and periodic scheduler to server

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,35 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"log"
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
 	app := fiber.New()
 
-	app.Get("/ping", func(c *fiber.Ctx) error {
-		return c.SendString("pong")
+	// Register routes
+	app.Get("/start", func(c *fiber.Ctx) error {
+		return c.SendString("Server started")
 	})
+
+	app.Get("/stop", func(c *fiber.Ctx) error {
+		return c.SendString("Server stopped")
+	})
+
+	app.Get("/sent-messages", func(c *fiber.Ctx) error {
+		return c.SendString("List of sent messages")
+	})
+
+	// Start the scheduler
+	ticker := time.NewTicker(2 * time.Minute)
+	go func() {
+		for range ticker.C {
+			log.Println("Processing all unsent messages...")
+		}
+	}()
 
 	app.Listen(":3000")
 }
